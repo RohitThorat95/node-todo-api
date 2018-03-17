b16dd367f5b16ba5542fee9168aae2bd80cae58c
RohitThorat95 <thorat.rohit95@gmail.com>
2018-03-17 12:35:19 +0530

    GET single todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -23,10 +24,6 @@ app.post('/todos', (req , res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on PORT : ${port}`);
-});
-
 // get todo
 app.get('/todos', (req,res) => {
   Todo.find().then( (todos) => {
@@ -35,5 +32,31 @@ app.get('/todos', (req,res) => {
     res.status(400).send(e);
   })
 })
+
+// Get specific todo
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then( (todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch( (e) => {
+    res.status(400).send();
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`App is listening on PORT : ${port}`);
+});
+
+
 
 module.exports = {app};
